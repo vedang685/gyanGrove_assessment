323536c11d57bce24170517e1b2eb6d3ee019ca7
Vedang <vedang685@gmail.com>
2024-07-01 22:41:52 +0530

    mobile responsive commit

--- src/navbar/NavBar.tsx
+++ src/navbar/NavBar.tsx
@@ -1,9 +1,9 @@
 import { Icons } from "@/components/Icons"
 import MaxWidthWrapper from "@/components/MaxWidthWrapper"
 import NavItems from "@/components/NavItems"
-import { buttonVariants } from "@/components/ui/button"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import Image from "next/image"
+import { Search } from 'lucide-react';
+import { Heart } from 'lucide-react';
+import { UserRound } from 'lucide-react';
 import Link from "next/link"
 import { FaHeart } from "react-icons/fa";
 
@@ -13,8 +13,8 @@ function NavBar() {
             <header className="relative bg-white">
                 <MaxWidthWrapper isNavbar ={true}>
                     <div className="border-b border-gray-200">
-                        <div className="flex gap-x-4 flex-row  h-24 items-center">
-                            <div className="w-[15%] justify-center items-center flex flex-col ml-4 lg:ml-0">
+                        <div className="flex gap-x-4 flex-row h-24 items-center">
+                            <div className="w-[30%] lg:w-[15%] xl:w-[15%] justify-center items-center flex flex-col ml-4 lg:ml-0">
                                 <Link href='/'>
                                   <span className=" ml-8 md:ml-6 lg:ml-2 text-logoColor font-bold">BookUsNow</span>
                                 </Link>
@@ -24,6 +24,17 @@ function NavBar() {
                                 </Link>
                                 <span className="text-[11px] ml-1 text-strokeColor font-bold">Mumbai, India</span>
                                 </div>
+                            </div>
+                            <div className="w-[80%] flex items-center gap-x-4  justify-end lg:hidden xl:hidden">
+                                <Link href='/'>
+                                    <Search color="#989090" className="h-5 w-6"/>
+                                </Link>
+                                <Link href='/'>
+                                    <Heart color="#989090" className="h-5 w-6"/>
+                                </Link>
+                                <Link href='/'>
+                                    <UserRound color="#989090" className="h-5 w-6"/>
+                                </Link>
                             </div>
                             <div className="w-[70%] hidden lg:flex">
                                 <NavItems/>
