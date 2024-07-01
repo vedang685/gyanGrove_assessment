d927a893f62a6653fac0f748d3aecef2729b7e4a
Vedang <vedang685@gmail.com>
2024-07-01 21:00:27 +0530

    responsive update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ function Page() {
         </div>
           <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50 flex items-center justify-center">
           </div>
-          <div className='absolute top-[40%] w-full flex flex-col items-center justify-center'>
+          <div className='absolute top-[20%] md:top-[40%] lg:top-[40%] w-full flex flex-col items-center justify-center'>
             <p className=" text-white text-3xl md:text-3xl lg:text-5xl font-bold text-center">Discover Exciting Events Happening</p>
             <p className=" text-white text-3xl md:text-3xl lg:text-5xl font-bold text-center"> Near You - Stay Tuned for Updates!</p>
             <p className="w-[80%] mt-4 text-white text-lg lg:text-xl md:text-lg text-center"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit facere at ullam dolorem, quaerat blanditiis repellendus officiis dignissimos sed, nostrum adipisci, consequatur error perferendis molestias dolore? Odit vel inventore quis.</p>
